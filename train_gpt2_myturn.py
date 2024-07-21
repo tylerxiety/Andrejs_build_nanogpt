8c93184e909f7efc136f0326ea0fda8cc4dc37b0
tyler <tylerxiety@gmail.com>
2024-07-21 22:37:10 +0800

    check loading the huggingface GPT2 param

---
+++ train_gpt2_myturn.py
@@ -0,0 +1,208 @@
+from dataclasses import dataclass
+import torch.nn as nn
+import math
+import torch
+
+
+# TDODO
+# watch/read attention code in previous video again
+
+
+
+class CausalSelfAttention(nn.Module):
+    # multi-headed attention
+    # optimized for efficiency in pytorch
+    # same naming convention to hugging face
+    def __init__(self, config):
+        super().__init__()
+        assert config.n_embd % config.n_head == 0
+        # key, query, value projections for all heads, but in a batch
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
+        # output projection
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1
+        # regularization
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+
+    def forward(self, x):
+        B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
+        # calculate query, key, values for all heads in batch and move head forward to be the batch dim
+        # nh is "number of heads", hs is "head size", and C (number of channels) = nh * hs
+        # e.g. in GPT-2 (124M), n_head=12, hs=64, so nh*hs=C=768 channels in the Transformer
+        qkv = self.c_attn(x)
+        q, k, v = qkv.split(self.n_embd, dim=2)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        # y = F.scaled_dot_product_attention(q, k, v, is_causal=True) # flash attention
+        # attension ( materializes the large (T, T) matrix for all the queries and keys)
+        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+       
+        att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))  # autoregressive mask
+        att = F.softmax(att, dim=1) # normalize
+
+        y = att @ v # weighted sum:(B, nh, T, T) X (B, nh, T, hs) -> (B, nh, T, hs)
+        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side. perform the concatenat ion function.
+        # output projection
+        y = self.c_proj(y)
+        return y
+    
+
+class MLP(nn.Module):
+    # nn.GELU(approximate='tanh') is an approximate version of GELU that was developed due to
+    # the error function erf was very slow in tensorflow back to years ago.
+    # nowadays we prefer use the exact version and there is no big difference.
+    # but back then GPT2 and BERT picked up the approximate version.
+    # we are reproducing GPT2 so using the approximate version here.
+
+    # why use GELU instead of RELU: because of the dead relu neuron problem 
+    # where any activations that fall on the flat tail at zero will get 
+    # zero gradient. But the GELU always contributes a local gradient.
+    # GELU empirically work better in practice as demonstrated in the GELU,
+    # GPT2 and BERT papers.
+
+    # more modern networks like LLAMA further use SwiGLU, etc.
+    # 
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc    = nn.Linear(config.n_embd, 4 * config.n_embd)
+        self.gelu    = nn.GELU(approximate='tanh')
+        self.c_proj  = nn.Linear(4 * config.n_embd, config.n_embd)
+        # self.c_proj.NANOGPT_SCALE_INIT = 1
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        return x
+    
+
+class Block(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.ln_1 = nn.LayerNorm(config.n_embd)
+        self.attn = CausalSelfAttention(config)
+        self.ln_2 = nn.LayerNorm(config.n_embd)
+        self.mlp = MLP(config)
+    # OG transformer: LN is after attn or FF, and 
+    # LN is inside the residual stream
+    # GPT2: clean residual stream from supervision all the way down to the inputs, 
+    # which is desirable, because the gradients from top flow straigt to the inputs unchanged.
+    # The attn is an aggregation func, weighted sum, like reduce
+    # whereas mlp happens at every single token individually, like map,
+    # so the transformer is like a repeteated application of map reduce.
+    def forward(self, x):
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
+    
+@dataclass
+class GPTConfig:
+    # block_size: int = 256 # max sequence length
+    # vocab_size: int = 65 # number of tokens: 50,000 BPE merges + 256 bytes tokens + 1 <|endoftext|> token
+    # n_layer: int = 6 # number of layers
+    # n_head: int = 6 # number of heads
+    # n_embd: int = 384 # embedding dimension
+
+    block_size: int = 1024 # max sequence length
+    vocab_size: int = 50257 # number of tokens: 50,000 BPE merges + 256 bytes tokens + 1 <|endoftext|> token
+    n_layer: int = 12 # number of layers
+    n_head: int = 12 # number of heads
+    n_embd: int = 768 # embedding dimension
+
+
+class GPT(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        # module transformer, the same to the transformer container in huggingface.
+        # nn.ModuleDict: so that we can index submodule using keys.
+        # nn.Embedding is just a fancy wraper around tensor.
+        # nn.ModuleList: so that we can index it using integers, same to the schema in huggingface.
+        # ln_f: GPT2 has a final LN at the end, which is different to the OG transformer paper.
+        # The other main difference is that LN is moved to before the MLP in GPT2.
+        # lm_head: GPT2 uses no bias in the final projection.
+        self.transformer = nn.ModuleDict(dict(
+            wte = nn.Embedding(config.vocab_size, config.n_embd),
+            wpe = nn.Embedding(config.block_size, config.n_embd),
+            h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
+            ln_f = nn.LayerNorm(config.n_embd),
+        ))
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+
+    def forward(self, idx, targets=None):
+        # idx is of shape (B, T)
+        B, T = idx.size()
+        assert T <= self.config.block_size, f"Cannot forward sequence of length {T}, block size is only {self.config.block_size}"
+        # forward the token and posisition embeddings
+        pos = torch.arange(0, T, dtype=torch.long, device=idx.device) # shape (T)
+        pos_emb = self.transformer.wpe(pos) # position embeddings of shape (T, n_embd)
+        tok_emb = self.transformer.wte(idx) # token embeddings of shape (B, T, n_embd)
+        x = tok_emb + pos_emb
+        # forward the blocks of the transformer
+        for block in self.transformer.h:
+            x = block(x)
+        # forward the final layernorm and the classifier
+        x = self.transformer.ln_f(x)
+        logits = self.lm_head(x) # (B, T, vocab_size)
+        loss = None
+        if targets is not None:
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
+        return logits, loss
+    
+    
+    @classmethod
+    def from_pretrained(cls, model_type):
+        """Loads pretrained GPT-2 model weights from huggingface"""
+        assert model_type in {'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'}
+        from transformers import GPT2LMHeadModel
+        print("loading weights from pretrained gpt: %s" % model_type)
+
+        # n_layer, n_head and n_embd are determined from model_type
+        config_args = {
+            'gpt2':         dict(n_layer=12, n_head=12, n_embd=768),  # 124M params
+            'gpt2-medium':  dict(n_layer=24, n_head=16, n_embd=1024), # 350M params
+            'gpt2-large':   dict(n_layer=36, n_head=20, n_embd=1280), # 774M params
+            'gpt2-xl':      dict(n_layer=48, n_head=25, n_embd=1600), # 1558M params
+        }[model_type]
+        config_args['vocab_size'] = 50257 # always 50257 for GPT model checkpoints
+        config_args['block_size'] = 1024 # always 1024 for GPT model checkpoints
+        # create a from-scratch initialized minGPT model
+        config = GPTConfig(**config_args)
+        model = GPT(config)
+        sd = model.state_dict()
+        sd_keys = sd.keys()
+        sd_keys = [k for k in sd_keys if not k.endswith('.attn.bias')] # discard this mask / buffer, not a param
+
+        # init a huggingface/transformers model
+        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+        sd_hf = model_hf.state_dict()
+
+        # copy while ensuring all of the parameters are aligned and match in names and shapes
+        sd_keys_hf = sd_hf.keys()
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.masked_bias')] # ignore these, just a buffer
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith('.attn.bias')] # same, just the mask (buffer)
+        transposed = ['attn.c_attn.weight', 'attn.c_proj.weight', 'mlp.c_fc.weight', 'mlp.c_proj.weight']
+        # basically the openai checkpoints use a "Conv1D" module, but we only want to use a vanilla Linear
+        # this means that we have to transpose these weights when we import them
+        assert len(sd_keys_hf) == len(sd_keys), f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
+        for k in sd_keys_hf:
+            if any(k.endswith(w) for w in transposed):
+                # special treatment for the Conv1D weights we need to transpose
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].t())
+            else:
+                # vanilla copy over the other parameters
+                assert sd_hf[k].shape == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
+
+        return model
+
+# -----------------------------------------------------------------------------
+model= GPT.from_pretrained('gpt2')
+print("didn't crash yay!")
